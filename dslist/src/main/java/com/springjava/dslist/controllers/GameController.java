69cf795fa54db9d1514b8b54d30547be3ca5f865
Makalister Honorato <makalisterhonorato@gmail.com>
2024-11-12 12:45:50 -0300

    Implementando modelos de dominio

--- dslist/src/main/java/com/springjava/dslist/controllers/GameController.java
+++ dslist/src/main/java/com/springjava/dslist/controllers/GameController.java
@@ -6,9 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.springjava.dslist.dto.GameMinDTO;
-import com.springjava.dslist.entities.Game;
 import com.springjava.dslist.services.GameService;
 
 @RestController
